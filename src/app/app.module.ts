ce6db058d758b3796d85135f3514159a06cfca06
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-11-28 17:35:52 +0300

    updated app to Angular v18 (new app builder, path build from dist/pokedex to dist/pokedex/browser, deprecated modules have been replaced with new provideHttpClient APIs)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { SharedModule } from './shared/shared.module'
-import { HttpClientModule } from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { CoreModule } from './core/core.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    CoreModule,
-  ],
-  providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, CoreModule],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class AppModule {}
