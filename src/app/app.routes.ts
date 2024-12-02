c24530247d86a4a7da771d148fbff8e23ca4ae8c
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-12-02 12:42:55 +0300

    switched AppComponent to standalone, platformBrowserDynamic switched to bootstrapApplication,

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
@@ -13,9 +12,3 @@ const routes: Routes = [
     loadComponent: () => import('./pokeball/pokeball.component').then(m => m.PokeballComponent),
   },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
