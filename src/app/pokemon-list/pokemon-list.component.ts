81efdaac9e7d7f0f7215d7937ba856e7a7df0aeb
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-12-02 13:49:47 +0300

    removed destroyService and its use in components

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,7 +1,6 @@
 import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { DestroyService } from '../core/services/destroy.service';
 
 import { Observable, takeUntil } from 'rxjs';
 import { PokemonWithImg } from '../core/models/pokemon.model';
@@ -16,7 +15,6 @@ import { CardComponent } from './components/card/card.component';
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss'],
-  providers: [DestroyService],
   imports: [
     CommonModule,
     SearchBarComponent,
@@ -26,7 +24,7 @@ import { CardComponent } from './components/card/card.component';
     CardComponent,
   ],
 })
-export class PokemonListComponent implements OnInit, OnDestroy {
+export class PokemonListComponent implements OnInit {
   private offset = 0;
   private pokemonsPerPage = 20;
   loading = false;
@@ -34,7 +32,6 @@ export class PokemonListComponent implements OnInit, OnDestroy {
   pokemonsCount!: Observable<number>;
   constructor(
     private pokemonService: PokemonService,
-    private destroyService: DestroyService,
     public dialog: MatDialog
   ) {}
 
@@ -42,16 +39,14 @@ export class PokemonListComponent implements OnInit, OnDestroy {
     this.loading = true;
     this.pokemonService
       .getPokemonList({ offset: this.offset, limit: this.pokemonsPerPage })
-      .pipe(takeUntil(this.destroyService.destory$$))
+      .pipe()
       .subscribe(pokemons => {
         this.pokemons = pokemons;
         this.loading = false;
       });
     this.pokemonsCount = this.pokemonService.countPokemons$$;
   }
-  ngOnDestroy() {
-    this.destroyService.destroySubscriptions();
-  }
+
   getPokemons(data: { pageSize?: number; pageIndex?: number; query?: string }) {
     this.loading = true;
     const newQuery = data.query ?? '';
@@ -62,7 +57,7 @@ export class PokemonListComponent implements OnInit, OnDestroy {
     this.pokemonsPerPage = newPokemonsPerPage;
     this.pokemonService
       .getPokemonList({ offset: newOffset, limit: newPokemonsPerPage, query: newQuery })
-      .pipe(takeUntil(this.destroyService.destory$$))
+      .pipe()
       .subscribe(pokemons => {
         this.pokemons = pokemons;
         this.loading = false;
--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { debounceTime, takeUntil, tap } from 'rxjs';
-import { DestroyService } from './../../../core/services/destroy.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
@@ -11,29 +10,25 @@ import { MatInputModule } from '@angular/material/input';
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
-  providers: [DestroyService],
   imports: [MatIconModule, MatFormFieldModule, ReactiveFormsModule, CommonModule, MatInputModule],
 })
-export class SearchBarComponent implements OnInit, OnDestroy {
+export class SearchBarComponent implements OnInit{
   searchControl = new FormControl('');
   @Output() searchEvent = new EventEmitter<{ query: string }>();
   constructor(
     private formBuilder: FormBuilder,
-    private destroyService: DestroyService
+    
   ) {}
   ngOnInit() {
     this.searchControl.valueChanges
       .pipe(
         debounceTime(1000),
         tap(searchText => this.searchEvent.emit({ query: searchText ?? '' })),
-        takeUntil(this.destroyService.destory$$)
       )
       .subscribe();
   }
   clear() {
     this.searchControl = this.formBuilder.control('');
   }
-  ngOnDestroy() {
-    this.destroyService.destroySubscriptions();
-  }
+
 }
